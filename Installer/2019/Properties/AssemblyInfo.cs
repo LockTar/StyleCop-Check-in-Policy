90be7a6bf2b0f1177d16276fcfcc62b7a9441a14
Ralph Jansen <ralph.jansen@outlook.com>
2019-04-09 16:14:12 +0300

    Update Installer/2019/Properties/AssemblyInfo.cs
    Co-Authored-By: tangelov <teodor.angelov@progress.com>

--- Installer/2019/Properties/AssemblyInfo.cs
+++ Installer/2019/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.4.0.0")]
 [assembly: AssemblyFileVersion("2.3.2.0")]
-[assembly: AssemblyInformationalVersion("2.3.2.0")]
+[assembly: AssemblyInformationalVersion("2.4.0.0")]
