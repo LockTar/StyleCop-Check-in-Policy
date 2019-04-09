d57967dcacb63e5b03f0a6448e52d565385f72e2
Ralph Jansen <ralph.jansen@outlook.com>
2019-04-09 16:01:27 +0300

    Update Installer/2019/Properties/AssemblyInfo.cs
    Co-Authored-By: tangelov <teodor.angelov@progress.com>

--- Installer/2019/Properties/AssemblyInfo.cs
+++ Installer/2019/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSIX2017")]
+[assembly: AssemblyTitle("VSIX2019")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
